d4351cda25ac934cf59f83c5bab51c977ed66fd7
Simo Mrdjen <dr.sizni@gmail.com>
2022-11-23 23:19:55 +0100

    first commit

---
+++ src/main/java/informacioni/sistem/apv/ApvApplication.java
@@ -0,0 +1,13 @@
+package informacioni.sistem.apv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/informacioni/sistem/apv/ApvApplicationTests.java
@@ -0,0 +1,13 @@
+package informacioni.sistem.apv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
